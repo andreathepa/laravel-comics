7d31b29476253844dc8d1c4b1c1e74f11a1a6755
andreathepa <andreadepa32@gmail.com>
2023-07-21 17:39:51 +0200

    jumbotron added

--- resources/views/partials/jumbo.blade.php
+++ resources/views/partials/jumbo.blade.php
@@ -5,17 +5,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel Comics</title>
-
-
     <!-- Styles -->
     @vite('resources/js/app.js')
 
 </head>
 
 <body>
-    @include('partials.header')
-    @include('partials.jumbo')
+
+    <div>
+        <div class="jumbotron">
+            <img src="{{ Vite::asset('resources/img/jumbotron.jpg')}}" alt="">
+        </div>
+       
+    </div>
+
+    
    
 </body>
 
