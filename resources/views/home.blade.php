3e79e4bc4e3fe7a3fe98d39a74f62d99ae6ea84c
andreathepa <andreadepa32@gmail.com>
2023-07-23 12:31:42 +0200

    added icons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
 
     <!-- Styles -->
     @vite('resources/js/app.js')
-
+    
 </head>
 
 <body>
@@ -18,13 +18,13 @@
     @include('partials.jumbo')
 
     <main>
-        <div class="container my-4">
+        <div class="container my-5">
             <div class="row">
                 @foreach($comics as $comic)
                 <div class="col-10 col-md-6 col-lg-2">
-                    <div class="card border-0 my-3">
-                        <img class="img-fluid card-img-top img-comic" src="{{ $comic['thumb'] }}" alt="{{ $comic['title']}}">
-                        <i class="my-2 text-uppercase">{{ $comic['title'] }}</i>
+                    <div class="card bg-transparent border-0 my-3">
+                        <img class="img-fluidcard-img-top img-comic" src="{{ $comic['thumb'] }}" alt="{{ $comic['title']}}">
+                        <i class="my-2 text-uppercase text-white">{{ $comic['title'] }}</i>
                     </div>
                 </div>
                 @endforeach
@@ -34,21 +34,30 @@
             <a>LOAD MORE</a>
         </div>
 
-        <div class="container_icons">
-            <div class="footer_blue flex justify-content-between align-items-center">
-                @foreach($icons as $icon)
+        <div class="container-fluid my-5 bg-primary bg-gradient">
+            <div class="row">
+                <div class="col">
+                    <div class="container p-5">
+                        <div class="row">
+                            <div class="col">
+                                <ul class="d-flex align-items-center">
+                                    @foreach($icons as $icon)
+                                        <li class="d-flex">
+                                            <div class="image_icon"><img src="{{ Vite::asset( $icon['icona'])}}" alt="{{ $icon['title']}}"></div>
+                                            <div class="text-white d-flex align-items-center mx-4">{{ $icon['title'] }}</div>
+                                        </li>
+                                    @endforeach
+                                </ul>
 
-                 <a class="flex align-items-center margin-left">
+                            </div>
+                        </div>
+                    </div>
 
-                    <div class="image_icon"><img src="{{ $icon['icona'] }}" alt="{{ $icon['title']}}"></div>
-                 </a>
-
-                 @endforeach
-
+                </div>
             </div>
         </div>
     </main>
-   
+    
 </body>
 
 </html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,59 +11,64 @@
 </head>
 
 <body>
-    <header class="container my-4">
-        <div class="row">
-            <div class="col-2">
-                <div class="d-flex justify-content-center">
-                    <img src="{{ Vite::asset('resources/img/dc-logo.png')}}">
 
+    <div class="container-fluid bg-primary p-2"></div>
+    <header class="container-fluid bg-white">
+
+        <div class="container p-3">
+            <div class="row">
+                <div class="col-2">
+                    <div class="d-flex justify-content-center">
+                        <img src="{{ Vite::asset('resources/img/dc-logo.png')}}">
+    
+                    </div>
+    
                 </div>
-
-            </div>
-            <div class="col">
-                <div class="nav d-flex justify-content-center my-3">
-                    <ul class="list-unstyled d-flex justify-content-center text-uppercase">
-                        
-                        <li class="nav-item mx-2">
-                            <a class="nav-link {{ Route::currentRouteName() === 'homepage' ? 'fw-bold' : '' }}" href="/">
-                                Homepage
-                            </a>
-                        </li>
-                        <li class="nav-item mx-2">
-                            <a class="nav-link {{ Route::currentRouteName() === 'link' ? 'fw-bold' : '' }}" href="/">
-                                Comics
-                            </a>
-                        </li>
-                        <li class="nav-item mx-2">
-                            <a class="nav-link {{ Route::currentRouteName() === 'link' ? 'fw-bold' : '' }}" href="/">
-                                Games
-                            </a>
-                        </li>
-                        <li class="nav-item mx-2">
-                            <a class="nav-link {{ Route::currentRouteName() === 'link' ? 'fw-bold' : '' }}" href="/">
-                                Movies
-                            </a>
-                        </li>
-                        <li class="nav-item mx-2">
-                            <a class="nav-link {{ Route::currentRouteName() === 'link' ? 'fw-bold' : '' }}" href="/">
-                                News
-                            </a>
-                        </li>
-                        <li class="nav-item mx-2">
-                            <a class="nav-link {{ Route::currentRouteName() === 'link' ? 'fw-bold' : '' }}" href="/">
-                                Blog
-                            </a>
-                        </li>
-                        <li>
-                            <div class="input-group rounded">
-                                <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-                            </div>
-                        </li>
-            
-                    </ul>
-
+                <div class="col">
+                    <div class="nav d-flex justify-content-center my-3">
+                        <ul class="list-unstyled d-flex justify-content-center text-uppercase">
+                            
+                            <li class="nav-item mx-2">
+                                <a class="nav-link {{ Route::currentRouteName() === 'homepage' ? 'fw-bold' : '' }}" href="/">
+                                    Homepage
+                                </a>
+                            </li>
+                            <li class="nav-item mx-2">
+                                <a class="nav-link {{ Route::currentRouteName() === 'link' ? 'fw-bold' : '' }}" href="/">
+                                    Comics
+                                </a>
+                            </li>
+                            <li class="nav-item mx-2">
+                                <a class="nav-link {{ Route::currentRouteName() === 'link' ? 'fw-bold' : '' }}" href="/">
+                                    Games
+                                </a>
+                            </li>
+                            <li class="nav-item mx-2">
+                                <a class="nav-link {{ Route::currentRouteName() === 'link' ? 'fw-bold' : '' }}" href="/">
+                                    Movies
+                                </a>
+                            </li>
+                            <li class="nav-item mx-2">
+                                <a class="nav-link {{ Route::currentRouteName() === 'link' ? 'fw-bold' : '' }}" href="/">
+                                    News
+                                </a>
+                            </li>
+                            <li class="nav-item mx-2">
+                                <a class="nav-link {{ Route::currentRouteName() === 'link' ? 'fw-bold' : '' }}" href="/">
+                                    Blog
+                                </a>
+                            </li>
+                            <li>
+                                <div class="input-group rounded">
+                                    <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+                                </div>
+                            </li>
+                
+                        </ul>
+    
+                    </div>
+    
                 </div>
-
             </div>
         </div>
     </header>
