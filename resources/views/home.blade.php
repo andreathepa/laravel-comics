49e6100cd8c545b85efea412f84965ac561e2833
andreathepa <andreadepa32@gmail.com>
2023-07-23 12:46:54 +0200

    footer added-exercise done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,7 +34,7 @@
             <a>LOAD MORE</a>
         </div>
 
-        <div class="container-fluid my-5 bg-primary bg-gradient">
+        <div class="container-fluid mt-5 bg-primary bg-gradient">
             <div class="row">
                 <div class="col">
                     <div class="container p-5">
@@ -57,6 +57,85 @@
             </div>
         </div>
     </main>
+
+    <footer>
+        <div>
+            <div class="footer_bg">
+                <div>
+                    <img src="{{ Vite::asset('./resources/img/footer-bg.jpg')}}" class="bg_img" alt="">
+                    <img src="../assets/img/dc-logo-bg.png" class="bg_logo" alt="">
+    
+                </div>
+                <div class="list_footer">
+                    <div class="list_one">
+                        <ul>
+                            <li><strong>Voce 1</strong></li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                            
+                        </ul>
+        
+                        <ul>
+                            <li><strong>Voce 2</strong></li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                            
+                        </ul>
+    
+                    </div>
+                    <div class="list_two">
+                        <ul>
+                            <li><strong>Voce 3</strong></li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                            
+                        </ul>
+                    </div>
+                    <div class="list_two">
+                        <ul>
+                            <li><strong>Voce 4</strong></li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                            <li>Voce</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="container_sign_up">
+               <div class="sign_up d-flex justify-content-between align-items-center">
+                    <div class="button_sign">
+                        SIGN-UP NOW!
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <div class="follow"><a href="">FOLLOW US</a></div>
+                        <ul>
+                            <li v-for="(social, index) in socials" :key="index">
+                                <a :href="social.href" :active="social.active ? 'active' : ''">
+                                    <div><img :src="social.logo_social" alt=""></div>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+               </div> 
+    
+           </div>
+        </div>
+    </footer>
     
 </body>
 
