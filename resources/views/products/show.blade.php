5cea2bce202493dab4917f9c8709e5c0d7088b72
andreathepa <andreadepa32@gmail.com>
2023-07-25 00:33:51 +0200

    style show.blade

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -13,7 +13,26 @@
                             <div class="content position relative">
                                 <img class="position-absolute top-50 start-30 show-image" src="{{ $comic['thumb'] }}" alt="{{ $comic['title']}}">
                                 <div>
-                                    <i class="text-uppercase">{{ $comic['title'] }}</i>
+                                    <h2 class="text-uppercase text-primary">{{ $comic['title'] }}</h2>
+                                    <div class="p-3 bg-success container">
+                                      <div class="row">
+                                        <div class="col-8">
+                                          <div class="row d-flex justify-content-between">
+                                            <div class="col d-flex justify-content-between">
+                                              U.S. Price: {{ $comic['price'] }}
+                                            </div>
+                                            <div class="col text-end text-uppercase">
+                                              Avaiable
+                                            </div>
+                                          </div>
+                                        </div>
+
+                                        <div class="col text-end">
+                                          Check aviability
+                                        </div>
+                                      </div>
+
+                                    </div>
                                     <p class="my-3">
                                         {{ $comic['description'] }}
                                     </p>
@@ -27,7 +46,7 @@
 
     </div>
 
-    <div class="container-fluid bg-light bg-gradient p-5">
+    {{-- <div class="container-fluid bg-light bg-gradient p-5">
         <div class="container my-5">
             <div class="row">
                 <div class="col-10 mx-auto">
@@ -86,5 +105,84 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
+
+    <div class="container-fluid bg-light bg-gradient p-5">
+      <div class="container my-5">
+          {{-- First row col Talent --}}
+          <div class="row">
+              <div class="col">
+                <h2 class="mb-4 text-primary px-3">Talent</h2>
+                <div>
+                  <div class="row border-top border-bottom p-3">
+                    {{-- First row col art by --}}
+                    <div class="col-3">
+                      <div class="fw-bold p-3">Art by:</div>
+                    </div>
+                    {{-- First row col art by description --}}
+                    <div class="col">
+                      <div class="p-3 text-info">
+                        @foreach (
+                          $comic['artists'] as $artist){{ $artist }}<br>
+                        @endforeach
+
+                      </div>
+                    </div>
+                  </div>
+
+                  {{-- Second row col written by --}}
+                  <div class="row border-bottom p-3">
+                    <div class="col-3">
+                      <div class="fw-bold p-3">Written by:</div>
+                    </div>
+                    {{-- Second row col written by description --}}
+                    <div class="col">
+                      <div class="p-3 text-info">
+                        @foreach (
+                          $comic['writers'] as $artist){{ $artist }}<br>
+                        @endforeach
+
+                      </div>
+                    </div>
+                    
+                  </div>
+                </div>
+              </div>
+
+              <div class="col">
+                <h2 class="mb-4 text-primary px-3">Specs</h2>
+                <div>
+                  <div class="row border-top border-bottom p-3">
+                    <div class="col-3">
+                      <div class="fw-bold p-3">Series:</div>
+                    </div>
+                    <div class="col">
+                      <div class="p-3 text-info">{{ $comic['series'] }}</div>
+                    </div>
+                  </div>
+
+                  {{-- Second row written by --}}
+                  <div class="row border-bottom p-3">
+                    <div class="col-3">
+                      <div class="fw-bold p-3">U.S. Price:</div> 
+                    </div>
+                    <div class="col">
+                      <div class="p-3 text-info">{{ $comic['price'] }}</div>
+                    </div>
+                    
+                  </div>
+                  <div class="row border-bottom p-3">
+                    <div class="col-3">
+                      <div class="fw-bold p-3">On sale date:</div> 
+                    </div>
+                    <div class="col">
+                      <div class="p-3 text-info">{{ $comic['sale_date'] }}</div>
+                    </div>
+                    
+                  </div>
+                </div>
+              </div>
+          </div>
+      </div>
+  </div>
 @endsection
